bbfb8cd5fbff230daa20dc012380e24462180b38
Kate Onishchenko <ek.onihenko@gmail.com>
2025-06-19 21:12:56 +0300

    динамич передача данных и testStep

---
+++ tests/models/MainPage.js
@@ -0,0 +1,11 @@
+class MainPage {
+  readonly page: Page;
+  readonly elements;
+
+  constructor(page: Page){
+    this.page = page
+    this.elements
+  }
+
+
+}
--- tests/pages/test-1.spec.js
+++ tests/pages/test-1.spec.js
@@ -1,8 +1,31 @@
 import { test, expect } from '@playwright/test';
 
+const elements = [
+  {
+    locator: (page) => page.getByRole('link', { name: ' Login' }),
+  },
+  {
+    locator: (page) => page.getByRole('banner').getByRole('link', { name: ' Source code' }),
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: ' Home' }),
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: ' Login' }),
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'Sign up' }),
+  },
+];
+
 test('Проверка отображения элементов навигации.Хедер', async ({ page }) => {
   await page.goto('https://realworld.qa.guru/#/');
-  await expect(page.getByRole('banner').getByRole('link', { name: 'conduit' })).toBeVisible();
+
+  for (const { locator } of elements) {
+    await expect(locator(page)).toBeVisible();
+  }
+
+  // Дополнительные проверки
   await expect(page.getByRole('banner').getByRole('link', { name: ' Source code' })).toBeVisible();
   await expect(page.getByRole('link', { name: ' Home' })).toBeVisible();
   await expect(page.getByRole('link', { name: ' Login' })).toBeVisible();
@@ -25,4 +48,25 @@ test('Успешная авторизация пользователя', async (
   await page.getByRole('textbox', { name: 'Password' }).fill('123456');
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByText('Kate')).toBeVisible();
+});
+
+//Использование Test-step на примере авторизации
+test('Авторизация пользователя', async ({ page }) => {
+  await test.step('Открыть страницу входа', async () => {
+    await page.goto('https://realworld.qa.guru/#/');
+    await page.getByRole('link', { name: ' Login' }).click();
+  });
+
+  await test.step('Заполнить поля авторизации', async () => {
+    await page.getByRole('textbox', { name: 'Email' }).fill('katrinka28i@mail.ru');
+    await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  });
+
+  await test.step('Отправить форму', async () => {
+    await page.getByRole('button', { name: 'Login' }).click();
+  });
+
+  await test.step('Проверить, что пользователь авторизован', async () => {
+    await expect(page.getByText('Kate')).toBeVisible();
+  });
 });
