f4a858cf53bf71e26f1af9ab539ad3fd01cba846
Kate Onishchenko <ek.onihenko@gmail.com>
2025-06-27 00:19:55 +0300

    Тест "Добавить комментарий"

---
+++ pom_tests/pages/addCom.js
@@ -0,0 +1,16 @@
+exports.AddComment = class AddComment {
+  constructor(page) {
+    this.page = page;
+    this.WriteComment_textbox = page.getByRole('textbox', { name: 'Write a comment...' });
+    this.PostComment_button = page.getByRole('button', { name: 'Post Comment' });
+  }
+
+  async gotoAddComment(articleSlug) {
+    await this.page.goto(`https://realworld.qa.guru/#/article/${articleSlug}`);
+  }
+
+  async addComment(writeComment) {
+    await this.WriteComment_textbox.fill(writeComment);
+    await this.PostComment_button.click();
+  }
+};
